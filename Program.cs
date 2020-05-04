a6d769808cc904017272586f1e28cd2b65c22d9d
Magnus Bjerke Vik <magnus@flytit.no>
2020-05-04 11:26:35 +0200

    Also test CosmoDB and write to CSV

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CsvHelper;
 using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.IdGenerators;
 using MongoDB.Driver;
 
 namespace mongo_id_perf
@@ -21,31 +23,59 @@ namespace mongo_id_perf
 
     class Program
     {
-        const int numInsertions = 1_000_000;
-        const int numRetrievals = 10_000;
+        const int numInsertions = 1_000;
+        static readonly int numRetrievals = numInsertions;
 
-        const string connectionString = "mongodb://localhost:27117";
+        const string dbName = "id-perf";
         const string collectionName = "documents";
 
         static async Task Main(string[] args)
         {
-            await RunOidTest();
-            await RunGuidTest();
+            var targets = new (string, string)[] {
+                ("mongodb local", "mongodb://localhost:27117"),
+                ("cosmosdb az", "REDACTED"),
+                ("cosmosdb az geo", "REDACTED"),
+            };
+
+            var results = new List<(string, List<double>)>(targets.Length);
+
+            foreach (var (targetName, targetConnectionString) in targets) {
+                var oidResults = await RunOidTest(targetName, targetConnectionString);
+                var guidResults = await RunGuidTest(targetName, targetConnectionString);
+
+                results.Add(($"{targetName} OID", oidResults));
+                results.Add(($"{targetName} GUID", guidResults));
+            }
+
+            using var writer = new StreamWriter("./results.csv");
+            using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+            foreach (var (name, values) in results) {
+                csv.WriteField(name);
+                foreach (var time in values) {
+                    csv.WriteField(time);
+                }
+                csv.NextRecord();
+            }
+            csv.Flush();
         }
 
-        static async Task<IMongoCollection<TDocument>> SetupCollection<TDocument>()
+        static async Task<IMongoCollection<TDocument>> SetupCollection<TDocument>(string targetConnectionString)
         {
-            var dbName = $"id-perf-{Guid.NewGuid()}";
-            var client = new MongoClient(connectionString);
+            var client = new MongoClient(targetConnectionString);
             var db = client.GetDatabase(dbName);
-            await db.CreateCollectionAsync(collectionName);
             var collection = db.GetCollection<TDocument>(collectionName);
+            await collection.DeleteManyAsync(x => true);
             return collection;
         }
 
-        static async Task RunOidTest()
+        static async Task<List<double>> RunOidTest(string targetName, string targetConnectionString)
         {
-            var collection = await SetupCollection<OidDocument>();
+            Console.WriteLine($"Running OID test against {targetName}");
+
+            var collection = await SetupCollection<OidDocument>(targetConnectionString);
+
+            Console.WriteLine("Inserting...");
 
             var documents = Enumerable.Range(0, numInsertions)
                 .Select(_ => new OidDocument())
@@ -60,23 +90,30 @@ namespace mongo_id_perf
                 .Select(_ => documentIds[rng.Next(0, documentIds.Length)])
                 .ToArray();
 
+            var results = new List<double>(idsToRetrieve.Length);
+
             var stopwatch = new Stopwatch();
-            stopwatch.Start();
+            var nanosPerTick = (1000L*1000L*1000L) / Stopwatch.Frequency;
+            Console.WriteLine($"High resolution swopwatch: {Stopwatch.IsHighResolution} ({nanosPerTick})");
 
             foreach (var id in idsToRetrieve)
             {
+                stopwatch.Restart();
                 await collection.Find(x => x.Id == id).FirstOrDefaultAsync();
+                stopwatch.Stop();
+                results.Add((double)stopwatch.ElapsedTicks * (double)nanosPerTick);
             }
 
-            stopwatch.Stop();
-            var timeSpent = stopwatch.Elapsed;
-
-            Console.WriteLine($"Retrievel of {numRetrievals} OID documents: {timeSpent}");
+            return results;
         }
 
-        static async Task RunGuidTest()
+        static async Task<List<double>> RunGuidTest(string targetName, string targetConnectionString)
         {
-            var collection = await SetupCollection<GuidDocument>();
+            Console.WriteLine($"Running GUID test against {targetName}");
+
+            var collection = await SetupCollection<GuidDocument>(targetConnectionString);
+
+            Console.WriteLine("Inserting...");
 
             var documents = Enumerable.Range(0, numInsertions)
                 .Select(_ => new GuidDocument())
@@ -91,18 +128,21 @@ namespace mongo_id_perf
                 .Select(_ => documentIds[rng.Next(0, documentIds.Length)])
                 .ToArray();
 
+            var results = new List<double>(idsToRetrieve.Length);
+
             var stopwatch = new Stopwatch();
-            stopwatch.Start();
+            var nanosPerTick = (1000L*1000L*1000L) / Stopwatch.Frequency;
+            Console.WriteLine($"High resolution swopwatch: {Stopwatch.IsHighResolution} ({nanosPerTick})");
 
             foreach (var id in idsToRetrieve)
             {
+                stopwatch.Restart();
                 await collection.Find(x => x.Id == id).FirstOrDefaultAsync();
+                stopwatch.Stop();
+                results.Add((double)stopwatch.ElapsedTicks * (double)nanosPerTick);
             }
 
-            stopwatch.Stop();
-            var timeSpent = stopwatch.Elapsed;
-
-            Console.WriteLine($"Retrievel of {numRetrievals} GUID documents: {timeSpent}");
+            return results;
         }
     }
 }
